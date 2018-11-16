117df58a0f8055cf96854ec4fe5ec03f68eda728
amason13 <ashmason1389@gmail.com>
2018-11-16 16:04:18 +0000

    Update my_darts_script.py

--- my_darts_script.py
+++ my_darts_script.py
@@ -2,43 +2,27 @@ from dartssim.match import Match
 from dartssim.player import Player
 
 #### Todays 1st match
-clayton = Player(name = 'Clayton', treble_pct=0.38, single_pct=0.522, big_miss_pct=0.038,small_miss_pct=0.061,
-                    bull_pct=0.3,outer_pct=0.55,double_pct=0.328)
+vandebergh = Player(name = 'Van de Bergh', treble_pct=0.385, single_pct=0.538, big_miss_pct=0.034,small_miss_pct=0.043,
+                    bull_pct=0.35,outer_pct=0.6,double_pct=0.354)
 
-ratajski = Player(name = 'Ratajski', treble_pct=0.371, single_pct=0.544, big_miss_pct=0.034,small_miss_pct=0.051,
-                    bull_pct=0.3,outer_pct=0.55,double_pct=0.354)
+sulijovic = Player(name = 'Sulijovic', treble_pct=0.407, single_pct=0.521, big_miss_pct=0.023,small_miss_pct=0.049,
+                    bull_pct=0.35,outer_pct=0.6,double_pct=0.395)
 
 #### 2nd match
-wade = Player(name = 'Wade', treble_pct=0.393, single_pct=0.545, big_miss_pct=0.024,small_miss_pct=0.038,
-                 bull_pct=0.35,outer_pct=0.6,double_pct=0.398)
+whitlock = Player(name = 'Whitlock', treble_pct=0.379, single_pct=0.542, big_miss_pct=0.03,small_miss_pct=0.049,
+                 bull_pct=0.35,outer_pct=0.6,double_pct=0.373)
 
-unterbuchner = Player(name = 'Unterbuchner')#,treble_pct=0.408, single_pct=0.521, big_miss_pct=0.026,small_miss_pct=0.045,
-                    #bull_pct=0.35,outer_pct=0.6,double_pct=0.362)
-
-#### 3rd match
-anderson = Player(name = 'Anderson', treble_pct=0.444, single_pct=0.471, big_miss_pct=0.035,small_miss_pct=0.05,
-                    bull_pct=0.35,outer_pct=0.6,double_pct=0.385)
-
-harms = Player(name = 'Harms')#, treble_pct=0.356, single_pct=0.56, big_miss_pct=0.03,small_miss_pct=0.055,
-                    #bull_pct=0.35,outer_pct=0.6,double_pct=0.344)
-
-#### 4th match
-smith = Player(name = 'Smith', treble_pct=0.419, single_pct=0.518, big_miss_pct=0.026,small_miss_pct=0.036,
-                    bull_pct=0.3,outer_pct=0.55,double_pct=0.371)
-
-vangerwen = Player(name = 'Van Gerwen', treble_pct=0.469, single_pct=0.474, big_miss_pct=0.026,small_miss_pct=0.03,
-                    bull_pct=0.3,outer_pct=0.55,double_pct=0.428)
+unterbuchner = Player(name = 'Unterbuchner',treble_pct=0.397, single_pct=0.507, big_miss_pct=0.037,small_miss_pct=0.058,
+                    bull_pct=0.35,outer_pct=0.6,double_pct=0.371)
 
 
 
 
-match1 = Match(clayton,ratajski)
-match2 = Match(wade, unterbuchner)
-match3 = Match(anderson, harms)
-match4 = Match(smith, vangerwen)
+match1 = Match(clayton,ratajski, first_to = 16)
+match2 = Match(wade, unterbuchner,first_to = 16)
+
 
 match1.simulate(num_sims = 100000)
-#match2.simulate(num_sims = 100000)
-#match3.simulate(num_sims = 100000)
-match4.simulate(num_sims = 100000)
+match2.simulate(num_sims = 100000)
+
 
