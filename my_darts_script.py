2ed7a2ab4ba846287cb671a2fb79db9cc2dd8cbf
amason13 <ashmason1389@gmail.com>
2018-11-16 16:05:30 +0000

    Update my_darts_script.py

--- my_darts_script.py
+++ my_darts_script.py
@@ -12,14 +12,14 @@ sulijovic = Player(name = 'Sulijovic', treble_pct=0.407, single_pct=0.521, big_m
 whitlock = Player(name = 'Whitlock', treble_pct=0.379, single_pct=0.542, big_miss_pct=0.03,small_miss_pct=0.049,
                  bull_pct=0.35,outer_pct=0.6,double_pct=0.373)
 
-unterbuchner = Player(name = 'Unterbuchner',treble_pct=0.397, single_pct=0.507, big_miss_pct=0.037,small_miss_pct=0.058,
+price = Player(name = 'Price',treble_pct=0.397, single_pct=0.507, big_miss_pct=0.037,small_miss_pct=0.058,
                     bull_pct=0.35,outer_pct=0.6,double_pct=0.371)
 
 
 
 
-match1 = Match(clayton,ratajski, first_to = 16)
-match2 = Match(wade, unterbuchner,first_to = 16)
+match1 = Match(vandebergh, sulijovic, first_to = 16)
+match2 = Match(whitlock, price,first_to = 16)
 
 
 match1.simulate(num_sims = 100000)
