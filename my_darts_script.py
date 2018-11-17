7823b9be115b0f760f7014a841fdf49ebb4782e0
amason13 <ashmason1389@gmail.com>
2018-11-17 18:54:39 +0000

    Update my_darts_script.py

--- my_darts_script.py
+++ my_darts_script.py
@@ -2,27 +2,27 @@ from dartssim.match import Match
 from dartssim.player import Player
 
 #### Todays 1st match
-vandebergh = Player(name = 'Van de Bergh', treble_pct=0.385, single_pct=0.538, big_miss_pct=0.034,small_miss_pct=0.043,
-                    bull_pct=0.35,outer_pct=0.6,double_pct=0.354)
+clayton = Player(name = 'Clayton', treble_pct=0.39, single_pct=0.512, big_miss_pct=0.036,small_miss_pct=0.062,
+                    bull_pct=0.35,outer_pct=0.6,double_pct=0.315)
 
-sulijovic = Player(name = 'Sulijovic', treble_pct=0.407, single_pct=0.521, big_miss_pct=0.023,small_miss_pct=0.049,
-                    bull_pct=0.35,outer_pct=0.6,double_pct=0.395)
-
+gerwen = Player(name = 'Van Gerwen', treble_pct=0.469, single_pct=0.474, big_miss_pct=0.026,small_miss_pct=0.03,
+                    bull_pct=0.35,outer_pct=0.6,double_pct=0.428)
+'''
 #### 2nd match
 whitlock = Player(name = 'Whitlock', treble_pct=0.379, single_pct=0.542, big_miss_pct=0.03,small_miss_pct=0.049,
                  bull_pct=0.35,outer_pct=0.6,double_pct=0.373)
 
 price = Player(name = 'Price',treble_pct=0.397, single_pct=0.507, big_miss_pct=0.037,small_miss_pct=0.058,
                     bull_pct=0.35,outer_pct=0.6,double_pct=0.371)
+'''
 
 
 
-
-match1 = Match(vandebergh, sulijovic, first_to = 16)
-match2 = Match(whitlock, price,first_to = 16)
+match1 = Match(clayton, gerwen, first_to = 16)
+#match2 = Match(whitlock, price,first_to = 16)
 
 
 match1.simulate(num_sims = 100000)
-match2.simulate(num_sims = 100000)
+#match2.simulate(num_sims = 100000)
 
 
