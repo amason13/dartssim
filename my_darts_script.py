8cda44a44ddba07a7e40c0b2af9779b94fcae50b
amason13 <ashmason1389@gmail.com>
2018-11-17 19:33:40 +0000

    Update my_darts_script.py

--- my_darts_script.py
+++ my_darts_script.py
@@ -2,11 +2,11 @@ from dartssim.match import Match
 from dartssim.player import Player
 
 #### Todays 1st match
-clayton = Player(name = 'Clayton', treble_pct=0.38, single_pct=0.522, big_miss_pct=0.038,small_miss_pct=0.061,
-                    bull_pct=0.35,outer_pct=0.6,double_pct=0.328)
+clayton = Player(name = 'Clayton', treble_pct=0.385, single_pct=0.515, big_miss_pct=0.038,small_miss_pct=0.062,
+                    bull_pct=0.35,outer_pct=0.6,double_pct=0.41)
 
 gerwen = Player(name = 'Van Gerwen', treble_pct=0.469, single_pct=0.474, big_miss_pct=0.026,small_miss_pct=0.03,
-                    bull_pct=0.35,outer_pct=0.6,double_pct=0.428)
+                    bull_pct=0.35,outer_pct=0.6,double_pct=0.455)
 '''
 #### 2nd match
 whitlock = Player(name = 'Whitlock', treble_pct=0.379, single_pct=0.542, big_miss_pct=0.03,small_miss_pct=0.049,
