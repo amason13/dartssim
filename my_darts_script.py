0da1a3921afbd2cdc3b582bb0ce6e5408e5a2a19
amason13 <ashmason1389@gmail.com>
2018-11-18 13:15:04 +0000

    Update my_darts_script.py

--- my_darts_script.py
+++ my_darts_script.py
@@ -2,18 +2,19 @@ from dartssim.match import Match
 from dartssim.player import Player
 
 #### Todays 1st match
-clayton = Player(name = 'Clayton', treble_pct=0.385, single_pct=0.515, big_miss_pct=0.038,small_miss_pct=0.062,
-                    bull_pct=0.35,outer_pct=0.6,double_pct=0.386)
+price = Player(name = 'Price',treble_pct=0.406, single_pct=0.492, big_miss_pct=0.041,small_miss_pct=0.062,
+                    bull_pct=0.35,outer_pct=0.6,double_pct=0.365)
 
-gerwen = Player(name = 'Van Gerwen', treble_pct=0.469, single_pct=0.474, big_miss_pct=0.026,small_miss_pct=0.03,
-                    bull_pct=0.35,outer_pct=0.6,double_pct=0.458)
+suljovic = Player(name = 'Sujovic', treble_pct=0.403, single_pct=0.528, big_miss_pct=0.023,small_miss_pct=0.045,
+                    bull_pct=0.35,outer_pct=0.6,double_pct=0.395)
 '''
 #### 2nd match
 whitlock = Player(name = 'Whitlock', treble_pct=0.379, single_pct=0.542, big_miss_pct=0.03,small_miss_pct=0.049,
                  bull_pct=0.35,outer_pct=0.6,double_pct=0.373)
+                 
+clayton = Player(name = 'Clayton', treble_pct=0.385, single_pct=0.515, big_miss_pct=0.038,small_miss_pct=0.062,
+                    bull_pct=0.35,outer_pct=0.6,double_pct=0.386)
 
-price = Player(name = 'Price',treble_pct=0.397, single_pct=0.507, big_miss_pct=0.037,small_miss_pct=0.058,
-                    bull_pct=0.35,outer_pct=0.6,double_pct=0.371)
 '''
 
 
