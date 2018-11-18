4c40898c05c0779113875516ad7b66967944dd5c
amason13 <ashmason1389@gmail.com>
2018-11-18 13:35:56 +0000

    Update my_darts_script.py

--- my_darts_script.py
+++ my_darts_script.py
@@ -7,23 +7,23 @@ price = Player(name = 'Price',treble_pct=0.406, single_pct=0.492, big_miss_pct=0
 
 suljovic = Player(name = 'Suljovic', treble_pct=0.403, single_pct=0.528, big_miss_pct=0.023,small_miss_pct=0.045,
                     bull_pct=0.35,outer_pct=0.6,double_pct=0.395)
-'''
+
 #### 2nd match
-whitlock = Player(name = 'Whitlock', treble_pct=0.379, single_pct=0.542, big_miss_pct=0.03,small_miss_pct=0.049,
-                 bull_pct=0.35,outer_pct=0.6,double_pct=0.373)
+gerwen = Player(name = 'van Gerwen', treble_pct=0.466, single_pct=0.478, big_miss_pct=0.026,small_miss_pct=0.029,
+                 bull_pct=0.35,outer_pct=0.6,double_pct=0.428)
                  
-clayton = Player(name = 'Clayton', treble_pct=0.385, single_pct=0.515, big_miss_pct=0.038,small_miss_pct=0.062,
-                    bull_pct=0.35,outer_pct=0.6,double_pct=0.386)
-
-'''
+anderson = Player(name = 'Anderson', treble_pct=0.449, single_pct=0.464, big_miss_pct=0.036,small_miss_pct=0.05,
+                    bull_pct=0.35,outer_pct=0.6,double_pct=0.423)
 
 
 
-match1 = Match(price, suljovic, first_to = 16)
-#match2 = Match(whitlock, price,first_to = 16)
 
 
-match1.simulate(num_sims = 100000)
-#match2.simulate(num_sims = 100000)
+#match1 = Match(price, suljovic, first_to = 16)
+match2 = Match(gerwen, anderson, first_to = 16)
+
+
+#match1.simulate(num_sims = 100000)
+match2.simulate(num_sims = 100000)
 
 
